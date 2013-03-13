7cb635d26e02a648668079ac049dbc2e1e3bae22
Ryan Walsh <walshry3@msu.edu>
2013-03-12 23:47:30 -0400

    Week 7 Assignments
    Includes lab, project and writing

---
+++ projects/final/java/java.js
@@ -0,0 +1,24 @@
+function hockey()
+{
+alert('The 1998 Red Wings Stanley Cup Championship inspired Ryan to play hockey! Ryan went on to play for 7 years.')
+}
+function col_fb()
+{
+alert('As a member of the Spartan Marching Band, Ryan performed in front of 60,000 fans at the innaugural Big Ten Championship Game!')
+}
+function mlb()
+{
+alert('Ryan attended the 2006 World Series in Detroit and received an autograph from pitcher Joel Zumaya!')
+}
+function col_bb()
+{
+alert('As a member of the Spartan Brass, Ryan witnessed Korie Lucious win a tournament game with a 3 pointer at the buzzer!')
+}
+function nfl()
+{
+alert('Ryan attended the game in which Lions wide receiver Calvin Johnson broke the single season receiving record!')
+}
+function nba()
+{
+alert('Ryan once met former Piston Chauncey Billups at a local KFC!')
+}
